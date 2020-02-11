6635c00f967106732db94dde807a757670570950
khai96_ <hvksmr1996@gmail.com>
2020-02-11 10:40:39 +0700

    Use Integer

--- factorial.hs
+++ factorial.hs
@@ -1,13 +1,13 @@
 #! /usr/bin/env runhaskell
 import Control.Arrow
 
-factorial :: (Eq p, Num p) => p -> p
+factorial :: Integer -> Integer
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
 
 facStr :: String -> String
 facStr str = str ++ " => " ++ s where
-  n = read str :: Int
+  n = read str
   y = factorial n
   s = show y
 
--- fibonacci.hs
+++ fibonacci.hs
@@ -2,14 +2,14 @@
 import Data.Function
 import Control.Arrow
 
-fibonacci :: (Eq a, Num a) => a -> a
+fibonacci :: Integer -> Integer
 fibonacci 0 = 1
 fibonacci 1 = 1
 fibonacci n = (fibonacci $ n - 1) + (fibonacci $ n - 2)
 
 fibStr :: String -> String
 fibStr str = str ++ " => " ++ res where
-  res = str & (read :: String -> Int) & fibonacci & show
+  res = str & read & fibonacci & show
 
 main :: IO ()
 main = interact $ lines
